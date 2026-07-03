bf66cec1871fffbccc9c900ef0132341b273ce73
Amir-bek2012 <amirsalokhiddinov2012@gmail.com>
2026-07-03 21:52:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// Telegram Mini App
+
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+
+const men = document.getElementById("men");
+const women = document.getElementById("women");
+
+
+// Мужской отдел
+
+function showMen(){
+
+    men.style.display = "block";
+    women.style.display = "none";
+
+    window.scrollTo({
+        top: men.offsetTop - 20,
+        behavior: "smooth"
+    });
+
+}
+
+
+// Женский отдел
+
+function showWomen(){
+
+    women.style.display = "block";
+    men.style.display = "none";
+
+    window.scrollTo({
+        top: women.offsetTop - 20,
+        behavior: "smooth"
+    });
+
+}
+
+
+// Записаться
+
+function bookMaster(master){
+
+    tg.sendData(JSON.stringify({
+
+        action: "book",
+        master: master
+
+    }));
+
+}
+
+
+// Возврат на главный экран
+
+function goHome(){
+
+    men.style.display = "none";
+    women.style.display = "none";
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+}
+
+
+// Закрыть приложение
+
+function closeApp(){
+
+    tg.close();
+
+}
